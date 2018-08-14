e6f3f0bcd0288a4645cf7c74be44fbb566013bef
Matthew Waters <matthew.w.z.92@outlook.com>
2018-08-14 17:35:54 -0400

    First day's work.

---
+++ ClassLibrary1/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Engine.Models
+{
+    public class Player
+    {
+       public String Name { get; set; }
+       public String CharacterClass { get; set; }
+       public int HitPoints { get; set; }
+       public int ExperiencePoints { get; set; }
+       public int Level { get; set; }
+       public int Credits { get; set; }
+
+    }
+}
---
+++ ClassLibrary1/ViewModels/GameSession.cs
@@ -0,0 +1,22 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession ()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Zebulon Pike";
+            CurrentPlayer.Credits = 1000000;
+            CurrentPlayer.CharacterClass = "Force-Sensitive Mandalorian";
+            CurrentPlayer.HitPoints = 50;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            
+
+        }
+    }
+}
